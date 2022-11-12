dbee7c0dd7e7508420b70f865a4b3017067f8d51
EzeeBen <ezee.ds94@gmail.com>
2022-11-11 21:59:09 -0300

    Proyecto Final

--- JS/proyecto.js
+++ JS/proyecto.js
@@ -1,95 +1,124 @@
-let totalCalculado = 0;
-let buffer = "0";
-let operacionAnterior;
+//cart
+let cartIcon = document.querySelector("#cart-icon");
+let cart = document.querySelector(".cart");
+let closeCart = document.querySelector("#close-cart");
 
-const pantalla = document.querySelector(".pantalla");
+//Abre changuito
+cartIcon.onclick = () =>{
+  cart.classList.add("active");
+};
+//cierra changuito
+closeCart.onclick = () =>{
+  cart.classList.remove("active");
+};
 
-function clickBtn(valor) {
-  if (isNaN(valor)) {
-    tomaOperador(valor);
-  } else {
-    tomaNumero(valor);
-  }
-  pantalla.innerText = buffer;
+//Codigo del changuito
+if(document.readyState == "loading"){
+  document.addEventListener("DOMContentLoaded", ready)
+}else{
+  ready();
 }
 
-function tomaOperador(operador) {
-  switch (operador) {
-    case "C":
-      buffer = "0";
-      totalCalculado = 0;
-      break;
-    case "=":
-      if (operacionAnterior === null) {
-      return;
-      }
-      limpiarOperacion(parseInt(buffer));
-      operacionAnterior = null;
-      buffer = totalCalculado;
-      totalCalculado = 0;
-      break;
-    case "←":
-      if(buffer.length ===1){
-        buffer= "0";
-      }
-      else{
-        buffer= buffer.substring(0, buffer.
-        length - 1);
-      }
-      break;
-    case "+":
-    case "−":
-    case "×":
-    case "÷":
-      cuenta(operador);
-      break;
+//Funciones
+function ready(){
+  //quita objetos del carrito
+  var removeCartButtons = document.getElementsByClassName("cart-remove")
+  //console.log(removeCartButtons)
+  for (var i=0; i<removeCartButtons.length; i++){
+    var button = removeCartButtons[i];
+    button.addEventListener("click", removeCartItem);//OJO ACA
   }
+  //modificador de cantidades
+  var quantityInputs = document.getElementsByClassName("cart-quantity");
+  for(var i=0; i<quantityInputs.length; i++){
+    var input = quantityInputs[i];
+    input.addEventListener("change", quantityChanged);
+  }
+  //añadir al chango
+  var addCart=document.getElementsByClassName("add-cart");
+  for(var i=0; i<addCart.length; i++){
+    var button = addCart[i];
+    button.addEventListener("click", addCartClicked);
+  }
+  // boton compra
+  document.getElementsByClassName("btn-buy")[0].addEventListener("click", buyButtonCLicked);
+}
+//boton compra
+function buyButtonCLicked(){
+  alert("Tu compra fue realizada");
+  var cartContent= document.getElementsByClassName("cart-content")[0];
+  while(cartContent.hasChildNodes()){
+    cartContent.removeChild(cartContent.firstChild);
+  }
+  updatetotal();
 }
 
-function cuenta(operador){
-  if(buffer === "0"){
+//Saca objetos del chango
+function removeCartItem(event){
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+  updatetotal();
+}
+
+//Modificaciones de cantidad
+function quantityChanged(event){
+  var input = event.target;
+  if(isNaN(input.value) || input.value <= 0){
+    input.value = 1;
+  }
+  updatetotal();
+}
+//Añadir al chango
+function addCartClicked(event){
+  var button = event.target;
+  var shopProducts = button.parentElement;
+  var title= shopProducts.getElementsByClassName("product-title")[0].innerText;
+  var price= shopProducts.getElementsByClassName("price")[0].innerText;
+  var productImg= shopProducts.getElementsByClassName("product-img")[0].src;
+  addProductToCart(title, price, productImg);
+  updatetotal();
+}
+function addProductToCart(title, price, productImg){
+  var cartShopBox = document.createElement("div");
+  cartShopBox.classList.add("cart-box")
+  var cartItems = document.getElementsByClassName("cart-content")[0];
+  var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
+  for(var i=0; i<cartItemsNames.length; i++){
+    if(cartItemsNames[i].innerText == title){
+    alert("Ya tenes esto en el chango");
     return;
+    }
   }
 
-  const bufferNoVisible= parseInt(buffer);
-  if(totalCalculado === 0){
-    totalCalculado = bufferNoVisible;
-  }
-  else{
-    limpiarOperacion(bufferNoVisible);
-  }
-  operacionAnterior=operador;
-  buffer= "0";
+var cartBoxContent = `
+                        <img src="${productImg}" alt="" class="cart-img">
+                        <div class="detail-box">
+                            <div class="cart-product-title">${title}</div>
+                            <div class="price">${price}</div>
+                            <input type="number" value="1" class="cart-quantity">
+                        </div>
+                        <!-- Remove cart -->
+                        <i class='bx bx-trash cart-remove'></i>`;
+cartShopBox.innerHTML = cartBoxContent;
+cartItems.append(cartShopBox);
+cartShopBox.getElementsByClassName("cart-remove")[0].addEventListener("click", removeCartItem);
+cartShopBox.getElementsByClassName("cart-quantity")[0].addEventListener("change", quantityChanged);
 }
 
-function limpiarOperacion(bufferNoVisible){
-  if(operacionAnterior === "+"){
-    totalCalculado += bufferNoVisible;
-  }
-  else if(operacionAnterior === "−"){
-    totalCalculado -= bufferNoVisible;
-  }
-  else if(operacionAnterior === "×"){
-    totalCalculado *= bufferNoVisible;
-  }
-  else if(operacionAnterior === "÷"){
-    totalCalculado /= bufferNoVisible;
-  }
-}
 
-function tomaNumero(numeros){
-  if(buffer === "0"){
-    buffer = numeros;
+//Modificar total
+function updatetotal(){
+  var cartContent = document.getElementsByClassName("cart-content")[0];
+  var cartBoxes = cartContent.getElementsByClassName("cart-box");
+  var total=0;
+  for (var i=0; i<cartBoxes.length; i++){
+    var cartBox = cartBoxes[i];
+    var priceElement = cartBox.getElementsByClassName("price")[0];
+    var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+    var price = parseInt(priceElement.innerText.replace("$", ""))
+    var quantity = quantityElement.value;
+    total = total + price * quantity;
   }
-  else{
-    buffer += numeros;
-  }
+    document.getElementsByClassName("total-price")[0].innerText ="$" + total;
+  
 }
-
-function inicio(){
-  document.querySelector(".calc-btns").addEventListener("click", function(event){
-    clickBtn(event.target.innerText);
-  })
-}
-
-inicio();
